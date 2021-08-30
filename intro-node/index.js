e9242a9521a63e525c4b371dad3d7688350ba94a
dinh cong dinh <zedpro2k@gmail.com>
2021-08-30 22:42:08 +0700

    intro to nodejs

---
+++ intro-node/index.js
@@ -0,0 +1,11 @@
+var superheroes = require("superheroes");
+
+var hero = superheroes.random();
+
+console.log(hero);
+
+var supervillains = require("supervillains");
+
+var villain = supervillains.random();
+
+console.log(villain);
