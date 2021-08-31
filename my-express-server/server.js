39f361d5967cda94bf56de6cdd2c25295f88f958
dinh cong dinh <zedpro2k@gmail.com>
2021-08-31 16:01:52 +0700

    express and get/post method

---
+++ my-express-server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res){
+  // browser return response
+  // res.send("hello");
+
+  // response html file
+  res.send("<h1>hello</h1>");
+});
+
+
+app.get("/contact", function(req, res){
+  res.send("Contact me at: example@gmail.com");
+});
+
+
+app.get("/about", function(req, res){
+  res.send("Contact me at: example@gmail.com");
+});
+
+app.get("/hobbies", function(req, res){
+  res.send("<ul><li>Code</li><li>Coffee</li><li>Eat</li></ul>");
+});
+
+
+app.listen(4000);
